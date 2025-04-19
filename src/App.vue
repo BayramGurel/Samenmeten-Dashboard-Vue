ea3c7729dcc04ee0e3c7440a60db88f9cf3157c0
BayramGurel <bayramgurel10@hotmail.com>
2025-04-19 15:10:07 +0200

    setup

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <SamenMetenDashboard/>
+</template>
+
+<script>
+import SamenMetenDashboard from './components/SamenMetenDashboard.vue'
+
+export default {
+  name: 'App',
+  components: {
+    SamenMetenDashboard
+  }
+}
+</script>
+
+<style>
+/* Your CSS here */
+</style>
