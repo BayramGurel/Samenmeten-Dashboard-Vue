807c058b3fa0924a97f4819f7f7f0b6f1569b68c
BayramGurel <bayramgurel10@hotmail.com>
2025-05-18 16:24:43 +0200

    controlpanel done

--- src/components/ControlsPanel.vue
+++ src/components/ControlsPanel.vue
@@ -1,77 +1,73 @@
 <template>
-  <div class="position-absolute start-0 shadow mt-3 ms-3 col-md-4 col-8 custom-div">
-    <div class="container bg-white rounded pb-3">
-      <div class="row mb-2">
-        <div class="col-12 text-center">
-          <img :src="logoSrc" class="img-fluid rounded d-block" title="Refresh website" @click="$emit('reloadPage')" style="max-width: 70%">
-          <h4 class="text-center text-primary-emphasis m-2">Dashboard voor Luchtkwaliteitsmetingen</h4>
-        </div>
-      </div>
-      <nav>
-        <div class="nav nav-tabs" role="tablist">
-          <button class="nav-link active fw-semibold" data-bs-toggle="tab" data-bs-target="#nav-leganda" type="button" role="tab" aria-selected="true">Legenda voor Luchtkwaliteit</button>
-          <button class="nav-link fw-semibold" data-bs-toggle="tab" data-bs-target="#nav-contact" type="button" role="tab" aria-selected="false">Contactgegevens</button>
-        </div>
-      </nav>
-      <div class="tab-content">
-        <div class="tab-pane fade show active" id="nav-leganda" role="tabpanel">
-          <h4 class="my-2 pt-2 text-center text-primary-emphasis">Luchtkwaliteit filters & legenda <i class="bi bi-funnel"></i></h4>
-          <div class="col-md-11 mx-auto form-floating">
-            <select v-model="propertyValue" class="form-select form-select-md text-primary fw-semibold text-center shadow-sm" id="floatingSelect" @input="emitUpdate" style="background: white !important;">
-              <option value="no2">Stikstofdioxide | N02</option>
-              <option value="pm10">Fijnstof 10microm | PM10</option>
-              <option value="pm25">Fijnstof 2.5 microm | PM2,5</option>
-            </select>
-            <label for="floatingSelect">Selecteer een type sensordata</label>
+  <div class="container-fluid" id="czoom">
+    <div class="position-absolute start-0 shadow mt-3 ms-3 col-md-4 col-8 custom-div">
+      <div class="container bg-white rounded pb-3">
+        <div class="row mb-2">
+          <div class="col-12 text-center">
+            <img :src="logoSrc" class="img-fluid rounded d-block" title="Refresh website" @click="$emit('reloadPage')" style="max-width: 70%">
+            <h4 class="text-center text-primary-emphasis m-2">Dashboard voor Luchtkwaliteitsmetingen</h4>
           </div>
-          <div class="row px-3 mt-2">
-            <div class="col-12">
-              <label for="timeSlider">Geselecteerd uur: {{ timeValue }}:00</label>
-              <input id="timeSlider" ref="timeSlider" class="form-control-range w-100" type="range" min="0" max="23" step="1" v-model.number="timeValue" @change="emitUpdate" @mousedown="stopSlider" :title="`Geselecteerd uur: ${timeValue}:00`">
+        </div>
+        <nav>
+          <div class="nav nav-tabs" role="tablist">
+            <button class="nav-link active fw-semibold" id="nav-leganda-tab" data-bs-toggle="tab" data-bs-target="#nav-leganda" type="button" role="tab" aria-controls="nav-leganda" aria-selected="true">Legenda voor Luchtkwaliteit</button>
+            <button class="nav-link fw-semibold" id="nav-contact-tab" data-bs-toggle="tab" data-bs-target="#nav-contact" type="button" role="tab" aria-controls="nav-contact" aria-selected="false">Contactgegevens</button>
+          </div>
+        </nav>
+        <div class="tab-content">
+          <div class="tab-pane fade show active" id="nav-leganda" role="tabpanel" aria-labelledby="nav-leganda-tab" tabindex="0">
+            <h4 class="my-2 pt-2 text-center text-primary-emphasis">Luchtkwaliteit filters & legenda <i class="bi bi-funnel"></i></h4>
+            <div class="col-md-11 mx-auto form-floating">
+              <select v-model="propertyValue" class="form-select form-select-md text-primary fw-semibold text-center shadow-sm" id="floatingSelect" @input="emitUpdate" style="background: white !important;">
+                <option class="fw-semibold" value="no2">Stikstofdioxide | N02</option>
+                <option class="fw-semibold" value="pm10">Fijnstof 10microm | PM10</option>
+                <option class="fw-semibold" value="pm25" selected>Fijnstof 2.5 microm | PM2,5</option>
+              </select>
+              <label for="floatingSelect">Selecteer een type sensordata</label>
             </div>
-          </div>
-          <div class="row pt-2 px-3">
-            <div class="col-md-6">
-              <div class="form-floating mb-2">
-                <input type="search" list="dayNamesList" v-model="selectedDay" class="form-control text-primary fw-semibold rounded shadow-sm" id="floatingInput" placeholder=" " @input="emitUpdate" @click="clearInput">
-                <label for="floatingInput">Selecteer een datum</label>
+            <div class="row px-3 mt-2">
+              <div class="col-12">
+                <label for="timeSlider">Geselecteerd uur: {{ timeValue }}:00</label>
+                <input id="timeSlider" ref="timeSlider" class="form-control-range w-100" type="range" min="0" max="23" step="1" v-model.number="timeValue" @change="emitUpdate" @mousedown="stopSlider" :title="`Geselecteerd uur: ${timeValue}:00`">
               </div>
-              <datalist id="dayNamesList">
-                <option v-for="(day, i) in dayNames" :key="i" :value="day">{{ day }}</option>
-              </datalist>
             </div>
-            <div class="col-md-6 text-center">
-              <button id="playSlider" type="button" class="btn py-2 fw-medium w-100" :class="buttonClass" @click="toggleSlider">
-                <i v-if="isPlaying" class="bi bi-pause-circle-fill"></i>
-                <i v-else class="bi bi-google-play"></i>
-                {{ isPlaying ? 'Pauzeren' : 'Afspelen' }}
-              </button>
+            <div class="row pt-2 px-3">
+              <div class="col-md-6">
+                <div class="form-floating mb-2">
+                  <input type="search" list="dayNamesList" v-model="selectedDay" class="form-control text-primary fw-semibold rounded shadow-sm" id="floatingInput" placeholder=" " @input="emitUpdate" @click="clearInput">
+                  <label for="floatingInput">Selecteer een datum</label>
+                </div>
+                <datalist id="dayNamesList">
+                  <option v-for="(day, i) in dayNames" :key="i" :value="day">{{ day }}</option>
+                </datalist>
+              </div>
+              <div class="col-md-6 text-center">
+                <button id="playSlider" type="button" class="btn py-2 fw-medium w-100" :class="buttonClass" @click="toggleSlider"><i v-if="isPlaying" class="bi bi-pause-circle-fill"></i><i v-else class="bi bi-google-play"></i>{{ isPlaying ? 'Pauzeren' : 'Afspelen' }}</button>
+              </div>
+            </div>
+            <p class="text-center">Concentratie {{ formattedProperty }} nk (µg/m³)</p>
+            <div class="row px-2 text-center text-light">
+              <div class="col-12 col-xl-6 pb-2 fw-medium" v-for="(value, idx) in legendaValues" :key="idx"><div class="rounded" :style="{ backgroundColor: colors[idx] }"><span class="Legenda">{{ value }}</span></div></div>
+            </div>
+            <div class="row pt-1">
+              <h6 class="col"><small class="text-muted">{{ concentrationValues[0] }} Lage concentratie</small></h6>
+              <h6 class="col text-end"><small class="text-muted">Hoge concentratie {{ concentrationValues[1] }}</small></h6>
             </div>
           </div>
-          <p class="text-center">Concentratie {{ formattedProperty }} nk (µg/m³)</p>
-          <div class="row px-2 text-center text-light">
-            <div class="col-12 col-xl-6 pb-2 fw-medium" v-for="(value, idx) in legendaValues" :key="idx">
-              <div class="rounded" :style="{ backgroundColor: colors[idx] }"><span class="Legenda">{{ value }}</span></div>
-            </div>
-          </div>
-          <div class="row pt-1">
-            <h6 class="col"><small class="text-muted">{{ concentrationValues[0] }} Lage concentratie</small></h6>
-            <h6 class="col text-end"><small class="text-muted">Hoge concentratie {{ concentrationValues[1] }}</small></h6>
-          </div>
-        </div>
-        <div class="tab-pane fade" id="nav-contact" role="tabpanel">
-          <div class="row p-2">
-            <div class="card">
-              <div class="card-body">
-                <h3 class="card-title text-center text-success border-bottom pb-2">Contactgegevens</h3>
-                <dl class="row">
-                  <dt class="col-sm-3 font-weight-bold text-success">Team</dt>
-                  <dd class="col-sm-9">Het Team Geo van de Provincie Zuid-Holland is verantwoordelijk voor het beheer en de toepassing van geografische informatie en technologie.</dd>
-                  <dt class="col-sm-3 font-weight-bold text-success">Contact</dt>
-                  <dd class="col-sm-9">Voor technische ondersteuning of feedback over de applicatie, kunt u contact opnemen via: <a href="mailto:teamgeo@pzh.nl" class="link-success link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover fw-semibold">teamgeo@pzh.nl</a></dd>
-                  <dt class="col-sm-3 font-weight-bold text-success">Postadres</dt>
-                  <dd class="col-sm-9">Provincie Zuid-Holland Contact Centrum<br>Postbus 90602 - 2509 LP Den Haag</dd>
-                </dl>
+          <div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab" tabindex="0">
+            <div class="row p-2">
+              <div class="card">
+                <div class="card-body">
+                  <h3 class="card-title text-center text-success border-bottom pb-2">Contactgegevens</h3>
+                  <dl class="row">
+                    <dt class="col-sm-3 font-weight-bold text-success">Team</dt>
+                    <dd class="col-sm-9">Het Team Geo van de Provincie Zuid-Holland is verantwoordelijk voor het beheer en de toepassing van geografische informatie en technologie.</dd>
+                    <dt class="col-sm-3 font-weight-bold text-success">Contact</dt>
+                    <dd class="col-sm-9">Voor technische ondersteuning of feedback over de applicatie, kunt u contact opnemen via: <a href="mailto:teamgeo@pzh.nl" class="link-success link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover fw-semibold">teamgeo@pzh.nl</a></dd>
+                    <dt class="col-sm-3 font-weight-bold text-success">Postadres</dt>
+                    <dd class="col-sm-9">Provincie Zuid-Holland Contact Centrum<br>Postbus 90602 - 2509 LP Den Haag</dd>
+                  </dl>
+                </div>
               </div>
             </div>
           </div>
@@ -85,12 +81,12 @@
 export default {
   name: 'ControlsPanel',
   props: {
-    dayNames: { type: Array, required: true },
-    formattedProperty: { type: String, required: true },
-    legendaValues: { type: Array, required: true },
-    colors: { type: Array, required: true },
-    concentrationValues: { type: Array, required: true },
-    logoSrc: { type: String, default: require('@/assets/PZH-logo.png') }
+    dayNames: {type: Array, required: true},
+    formattedProperty: {type: String, required: true},
+    legendaValues: {type: Array, required: true},
+    colors: {type: Array, required: true},
+    concentrationValues: {type: Array, required: true},
+    logoSrc: {type: String, default: require('@/assets/PZH-logo.png')}
   },
   data() {
     return {
@@ -108,7 +104,7 @@ export default {
   },
   methods: {
     emitUpdate() {
-      this.$emit('updateLayer', { property: this.propertyValue, time: this.timeValue, date: this.selectedDay });
+      this.$emit('updateLayer', {property: this.propertyValue, time: this.timeValue, date: this.selectedDay});
     },
     toggleSlider() {
       this.isPlaying = !this.isPlaying;
@@ -120,7 +116,6 @@ export default {
       const maxHour = this.selectedDay === today ? new Date().getHours() : 23;
       this.timeValue = 0;
       this.emitUpdate();
-      this.isPlaying = true;
       this.interval = setInterval(() => {
         if (this.timeValue < maxHour) {
           this.timeValue++;
